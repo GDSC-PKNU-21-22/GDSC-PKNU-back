f8115256e80a3c73af621b8a777b0008af520500
JeongeunChoi <cju4103@gmail.com>
2022-05-18 09:36:15 +0900

    Issue #2 chore: application.properties 추가

---
+++ src/main/java/pkgdsc/pkgdscbackend/PkgdscBackendApplication.java
@@ -0,0 +1,13 @@
+package pkgdsc.pkgdscbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkgdscBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkgdscBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pkgdsc/pkgdscbackend/PkgdscBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pkgdsc.pkgdscbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkgdscBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
